fe793c8b07bfcdeb68727849e195aa5e03e4fa36
Agampal Singh <agampalsingh61@gmail.com>
2026-03-01 09:29:36 +0530

    Added Files

---
+++ src/main/java/com/agam/rest_example/Controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.agam.rest_example.Controller;
+
+import com.agam.rest_example.Model.Student;
+import com.agam.rest_example.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    @Autowired
+    private StudentService service;
+
+    @GetMapping("/get-all")
+    public List<Student> getStudents() {
+        return service.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable int id) {
+        return service.getStudentById(id);
+    }
+
+    @PostMapping("/add-student")
+    public Student addStudent(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable int id) {
+        return service.deleteStudentById(id);
+    }
+}
