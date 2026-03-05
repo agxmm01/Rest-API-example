e1ae0a643e35c942f4a6e7d6e7e1d3d30a989b65
Agampal Singh <agampalsingh61@gmail.com>
2026-03-05 13:47:48 +0530

    Update Student Feature Added

--- src/main/java/com/agam/rest_example/Service/StudentService.java
+++ src/main/java/com/agam/rest_example/Service/StudentService.java
@@ -35,7 +35,8 @@ public class StudentService {
 
     public ResponseEntity<Student> updateStudent(int id , Student student) {
         Student existingStudent = repository.findStudentById(id).orElseThrow(() ->
-                new RuntimeException("No student exist with this id"));
+                new RuntimeException("No student exist with this id"));  // Use any exception for your benefit but you need to handle it
+        // as we are using Optional<T> in Repository so exception handling is must.
         existingStudent.setName(student.getName());
         existingStudent.setCourse(student.getCourse());
         repository.save(existingStudent);
